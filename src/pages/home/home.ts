22a635027ff2c91f17056f80aa12936f3a1b8b7d
Roo Kaplan <roo263@hotmail.com>
2018-07-09 17:31:01 +0200

    home page update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,32 +18,34 @@ import { MarkerTypeId, IMapOptions } from 'angular-maps';
 export class HomePage {
   posts: any;
 
-
-
   public shelters: Array<Shelter> = [];
 
   constructor(public navCtrl: NavController, public http: Http) {
 
     this.http.get('https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=[AqBGcbdM62R25R1aIk5BYDur1Y6Ywp2AdtN2w2QZZIONHhZaJh10cLT6hw9U2AQ-]').map(res => res.json()).subscribe(data => {
       this.posts = data.data.children; // this is whats intergrated 
+   
     });
 
     var shelter1 = new Shelter();
-    shelter1.image = "shelter pic"
-    shelter1.name = "shelter name";
-    shelter1.address = "shelter address";
-    shelter1.note = "shelter description";
+    shelter1.image = " "
+    shelter1.name = "Roo's shelter";
+    shelter1.address = "1 Shelter Ave, Cape Town";
+    shelter1.note = "Homeless shelter";
     shelter1.link = "#";
+    shelter1.lat = -33.9249
+    shelter1.lng = 18.4241
     this.shelters.push(shelter1);
 
     var shelter2 = new Shelter();
-    shelter2.image = "shelter pic Mariel"
-    shelter2.name = "Mariels shelter";
-    shelter2.address = "shelter address";
-    shelter2.note = "homeless shelter";
+    shelter2.image = " "
+    shelter2.name = "Mariel's shelter";
+    shelter2.address = " 22 Shelter Rd, Milnerton, Cape Town";
+    shelter2.note = "Food Bank";
     shelter2.link = "#";
+    shelter2.lat = -33.12
+    shelter2.lng = 18.4241
     this.shelters.push(shelter2);
-
 
   }
    _markerTypeId = MarkerTypeId 
