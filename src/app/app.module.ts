7037c00e9c7891c9fbab25175b0ba79bfd3280b7
Roo Kaplan <roo263@hotmail.com>
2018-07-09 12:00:49 +0200

    maps working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,44 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MapModule, MapAPILoader, BingMapAPILoaderConfig, BingMapAPILoader, WindowRef, DocumentRef, MapServiceFactory, BingMapServiceFactory } from "angular-maps";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    MapModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: MapAPILoader, deps: [], useFactory: MapServiceProviderFactory}
   ]
 })
 export class AppModule {}
+
+export function MapServiceProviderFactory(){
+  let bc: BingMapAPILoaderConfig = new BingMapAPILoaderConfig();
+  bc.apiKey ="AqBGcbdM62R25R1aIk5BYDur1Y6Ywp2AdtN2w2QZZIONHhZaJh10cLT6hw9U2AQ-"; // your bing map key
+  bc.branch = "experimental"; 
+      // to use the experimental bing brach. There are some bug fixes for
+      // clustering in that branch you will need if you want to use 
+      // clustering.
+  return new BingMapAPILoader(bc, new WindowRef(), new DocumentRef());
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,32 @@
+
+// <reference path="../../../node_modules/types/bingmaps/MicrosoftMaps/CustomMapStyles.d.ts" />
+// <reference path="types/MicrosoftMaps/Microsoft.Maps.d.ts" />
+// <reference path="types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Shelter } from '../home/Models'
+import { Http } from '@angular/http'; //for intergrating 
+import 'rxjs/add/operator/map';
+import { MarkerTypeId, IMapOptions } from 'angular-maps';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: any;
+
+
 
   public shelters: Array<Shelter> = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get('https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=[AqBGcbdM62R25R1aIk5BYDur1Y6Ywp2AdtN2w2QZZIONHhZaJh10cLT6hw9U2AQ-]').map(res => res.json()).subscribe(data => {
+      this.posts = data.data.children; // this is whats intergrated 
+    });
 
     var shelter1 = new Shelter();
     shelter1.image = "shelter pic"
@@ -20,6 +36,29 @@ export class HomePage {
     shelter1.link = "#";
     this.shelters.push(shelter1);
 
+    var shelter2 = new Shelter();
+    shelter2.image = "shelter pic Mariel"
+    shelter2.name = "Mariels shelter";
+    shelter2.address = "shelter address";
+    shelter2.note = "homeless shelter";
+    shelter2.link = "#";
+    this.shelters.push(shelter2);
+
+
   }
+   _markerTypeId = MarkerTypeId 
+            // a little trick so we can use enums in the template...
+
+        _options: IMapOptions = {
+            disableBirdseye: false,
+            disableStreetside: false,
+            navigationBarMode: 1
+       };
+            // for all available options for the various components, see IInfoWindowOptions, IInfoWindowAction, IMarkerOptions, IMapOptions, IMarkerIconInfo
+
+        _click(){
+           console.log("hello world...");
+       }
+
 
 }
