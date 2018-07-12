7fbd9917e4d264303cd2a1290b5a6f355b73df8b
Mariel Green <marielgreen@Mariels-MacBook-Pro.local>
2018-07-12 16:19:53 +0200

    picturesgit add .git add .git add .

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { RegisterPage } from '../register/register';
+import { Http } from '@angular/http';
 
 
 /**
@@ -18,7 +19,7 @@ import { RegisterPage } from '../register/register';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   }
 
  navigateToHome() {
@@ -29,5 +30,6 @@ navigateToRegister(){
   console.log("Navigating...");
   this.navCtrl.push(RegisterPage)
 }
+
 
 }
